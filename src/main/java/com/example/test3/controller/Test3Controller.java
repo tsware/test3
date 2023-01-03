e1c1bb0f6b125d5f1a53803143ebc9e4a0e25dac
ohwhankwon <ohk239@tsware.co.kr>
2023-01-03 10:48:16 +0900

    test3 삭제

---
+++ src/main/java/com/example/test3/controller/Test3Controller.java
@@ -0,0 +1,14 @@
+package com.example.test3.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Test3Controller {
+        @GetMapping("/test3")
+        public String test() {
+            return "Hello World Auto Build Test";
+        }
+}
